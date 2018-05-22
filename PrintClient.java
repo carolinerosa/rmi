290f0316d850a9a6769e4ed78cccbe4ae9e0fc5d
RodrigoLugao <rodrigolugao@mail.com>
2018-05-22 09:31:49 -0300

    Jobs

--- PrintClient.java
+++ PrintClient.java
@@ -1,9 +1,14 @@
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class PrintClient
 {
+	
+	private int id;
+	private ArrayList<Job> jobs = new ArrayList();
+	
     private PrintClient() 
 	{}
 
@@ -15,15 +20,37 @@ public class PrintClient
 
             Registry registry = LocateRegistry.getRegistry(host);
 			PrintServerInterface stub = (PrintServerInterface) registry.lookup("PrintServer");
-
-			Scanner scanner = new Scanner( System.in );
-			System.out.print( "Digite o que voce quer imprimir: " );
-			String input = scanner.nextLine();
 			
-            String resposta = stub.requestPrint(input);
-			// Aqui a gente exibe o fruto produzido por esse lindo e maravilho sistema distribuído
-			// Passo importante 4: Verificar resposta dada pelo método remoto.
-            System.out.println("resposta: " + resposta);
+			id = stub.login();
+			System.out.println( "Logado no servidor com o id: "  + id);
+			
+			Scanner scanner = new Scanner( System.in );
+			String input;
+			
+			String caso = "p";
+			boolean sair = false;
+			
+			while(!sair){
+				switch(caso){
+					case "p" : 
+						System.out.print( "Digite o que voce quer imprimir: " );
+						input = scanner.nextLine();
+						
+						Job resposta = stub.requestPrint(input, id);
+						jobs.add(resposta);
+						// Aqui a gente exibe o fruto produzido por esse lindo e maravilho sistema distribuído
+						// Passo importante 4: Verificar resposta dada pelo método remoto.
+						System.out.println("resposta: " + resposta);
+						sair = true;
+						break;
+					//case "s" 
+					
+					default: 
+						break;
+				}
+			}
+
+			
         } 
 		catch (Exception e) // Esse catch trata os RemoteException caso ocorra algum
 		{
