ecebf7d108782924f2fabc15a3bd98dda09e590d
RodrigoLugao <rodrigolugao@mail.com>
2018-05-21 23:13:15 -0300

    Adicionado input

--- PrintClient.java
+++ PrintClient.java
@@ -1,5 +1,6 @@
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.Scanner;
 
 public class PrintClient
 {
@@ -15,9 +16,11 @@ public class PrintClient
             Registry registry = LocateRegistry.getRegistry(host);
 			PrintServerInterface stub = (PrintServerInterface) registry.lookup("PrintServer");
 
+			Scanner scanner = new Scanner( System.in );
+			System.out.print( "Digite o que voce quer imprimir: " );
+			String input = scanner.nextLine();
 			
-			
-            String resposta = stub.requestPrint("");
+            String resposta = stub.requestPrint(input);
 			// Aqui a gente exibe o fruto produzido por esse lindo e maravilho sistema distribuído
 			// Passo importante 4: Verificar resposta dada pelo método remoto.
             System.out.println("resposta: " + resposta);
