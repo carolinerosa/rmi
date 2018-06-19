ceaa8da1aaa306d292971bbec6fdba86e5224487
carolinerosa <carolinerosa@id.uff.br>
2018-06-18 21:29:31 -0300

    Update PrintServerInterface.java

--- PrintServerInterface.java
+++ PrintServerInterface.java
@@ -5,5 +5,6 @@ public interface PrintServerInterface extends Remote
 {
 	// Metodo em que um usuario solicita que um arquivo seja impresso
     Job requestPrint(String s, int c) throws RemoteException;
+	int getPosition(int j) throws RemoteException; 
 	int login() throws RemoteException;
 }
